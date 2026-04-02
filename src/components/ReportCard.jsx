05a03cac0d8f8f819d5af513b2486bfda0e8c45f
Caleb Steckler <stecklec@email.sc.edu>
2026-04-02 18:00:56 -0400

    Added Backend Reports

--- src/components/ReportCard.jsx
+++ src/components/ReportCard.jsx
@@ -1,16 +1,34 @@
 import "../css/ReportCard.css";
-import reportIcon from "../images/report-icon.png";
+import { useState } from "react";
 
 const ReportCard = (props) => {
+    const [open, setOpen] = useState(false);
+
     return (
-        <div className="report-card">
-            <img className="report-icon" src={props.img} alt="" />
-            <h2>{props.title}</h2>
-            <p>{props.author}</p>
-            <p>{props.description}</p>
-            <button>Read Report</button>
-        </div>
+        <>
+            <div className="report-card" onClick={() => setOpen(true)}>
+                <img className="report-icon" src={props.img}/>
+                <h2>{props.title}</h2>
+                <p>{props.author}</p>
+                <p>{props.description}</p>
+                <button type="button">Read Report</button>
+            </div>
+
+            {open && (
+                <div
+                    className="report-card-modal-backdrop"
+                    onClick={() => setOpen(false)}
+                >
+                    <div className="report-card-modal">
+                        <h2>{props.title}</h2>
+                        <p>{props.author}</p>
+                        <p>{props.description}</p>
+                        <button type="button" onClick={() => setOpen(false)}>Close</button>
+                    </div>
+                </div>
+            )}
+        </>
     );
-}
+};
 
 export default ReportCard;
